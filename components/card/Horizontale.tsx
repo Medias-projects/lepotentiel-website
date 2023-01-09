18308a12a51e65c27ee041c970c60e4fe1b7dc27
mechack tambwe <mesutarso@users.noreply.github.com>
2023-01-10 00:06:27 +0100

    fix bug

--- components/card/Horizontale.tsx
+++ components/card/Horizontale.tsx
@@ -29,7 +29,7 @@ function Horizontale({ title, category, image, uri, slug }: Props) {
           />
         </div>
         <div className="horizontale-card__content  flex flex-col justify-center">
-          <p className="horizontale-card__category bg-primary w-1/2 uppercase  text-white text-center font-semibold py-1 px-2 mb-2 text-sm ">
+          <p className="horizontale-card__category bg-primary w-2/3 md:w-1/2 uppercase  text-white text-center font-semibold py-1 px-2 mb-2 text-sm ">
             {category}
           </p>
           <h3 className="horizontale-card__title text-md text-black font-semibold mb-2 line-clamp-2 ">
